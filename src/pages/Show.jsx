5c86a780f52768075f386db2badc55d8a770f8e0
vbangs <veronicabangs@gmail.com>
2021-05-24 19:43:11 -0400

    Make changes to Show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import {useState} from "react"
 
-function Show(props){
-    return(
-        <h1>Show</h1>
+function Show(props) {
+    const id = props.match.params.id
+    // copied from people app
+    const books = props.books
+    const book = books.find((p) => {
+        return p._id === id
+    })
+
+    // state for book
+    const [editForm, setEditForm] = useState(book)
+
+    // handleChange function for comments form
+    const handleChange = (event) => {
+        setEditForm({
+            ...editForm,
+            [events.target.names]: event.target.value
+        })
+    }
+
+    // handleSubmit for book
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.updateBooks(editForm, book._id)
+        props.history.push("/show")
+    }
+
+    return (
+        <div className="book">
+            <h1>{book.title}</h1>
+            <h2>{book.authors}</h2>
+            <img src={book.image} alt={book.imageLinks.thumbnail} />
+            <a href="/Details"><button id="details">
+                Details
+            </button></a>
+        
+        </div>
     )
+
 }
+
 
 export default Show
