1fd7dd57e988a991451b19fec021bc44727cb416
System Administrator <root@Humairas-Air.attlocal.net>
2021-05-26 18:07:13 -0500

    edit page

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,2 +1,38 @@
 import React from 'react'
 import {useState} from "react"
+
+function updateComments(commentText) {
+    this.setState({commentText: comment.text.target.value})
+
+
+//state for our updateComment form
+const [editComment, setEditComment] = useState(commentText)
+setEditComment({
+    ...editComment,
+    [event.target.comment]: event.target.text
+})
+
+}
+
+//handleSubmit when an updateComment form is submitted
+const handleSubmit = (event) => {
+    //to prevent refresh
+    event.preventDefault()
+    //update the comment
+    commentText.updateComment(editComment, comment._id)
+    //redirect to index page
+    PaymentResponse.history.push("/")
+}
+
+const removeComment = () => {
+    commentText.deleteComment(comment._id)
+    commentText.history.push("/")
+}
+
+return <div>
+    <h1>{book.volumeInfo.title}</h1>
+    <h3>{book.volumeInfo.authors}</h3>
+    <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.imageLinks.thumbnail}/>
+    <textarea name="Comments" id="" cols="30" rows="10"></textarea>
+</div>
+export default Edit
